02f07b58b40b2b20e69394f50fa4ad1a9277011f
Kaique Covo <kaique.kng@gmail.com>
2022-07-24 15:31:05 -0300

    feature: add newTask on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import todoLogo from '../../assets/todo-logo.svg';
+import { NewTask } from '../index';
 import styles from './index.module.scss';
-
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={todoLogo} alt="Logo" />
+      <img src={todoLogo} alt='Logo' />
+      <div className={styles.task}>
+        <NewTask />
+      </div>
     </header>
   );
 }
