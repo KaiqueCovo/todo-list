7363967e39b458e6194d481c5ee2d5a72b251e32
Kaique Covo <kaique.kng@gmail.com>
2022-07-24 16:16:54 -0300

    refactor: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Header } from './components';
+import styles from './App.module.scss';
+import { Header, NewTask, Tasks } from './components';
 
 function App() {
-
   return (
     <div>
       <Header />
+      <main className={styles.wrapper}>
+        <NewTask />
+        <div className={styles.tasksList}>
+          <Tasks />
+        </div>
+      </main>
     </div>
   );
 }
