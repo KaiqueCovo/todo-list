3d0257ce8116ef114b89525f0e1150fb56e7c5bf
Kaique Covo <kaique.kng@gmail.com>
2022-07-24 16:14:18 -0300

    refactor: remove newTask component to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,10 @@
 import todoLogo from '../../assets/todo-logo.svg';
-import { NewTask } from '../index';
 import styles from './index.module.scss';
 
 export function Header() {
   return (
     <header className={styles.header}>
       <img src={todoLogo} alt='Logo' />
-      <div className={styles.task}>
-        <NewTask />
-      </div>
     </header>
   );
 }
