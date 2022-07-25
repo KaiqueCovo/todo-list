dbaebc472ac313521bb14a4ca472a6d392b012c0
Kaique Covo <kaique.kng@gmail.com>
2022-07-24 21:15:23 -0300

    feature: handleSubmit and invalid

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,14 +1,44 @@
+import { ChangeEvent, createRef, FormEvent, InvalidEvent } from 'react';
+
 import { PlusCircle } from 'phosphor-react';
 
 import styles from './index.module.scss';
 
 export function NewTask() {
+  const inputRef = createRef<HTMLInputElement>();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!inputRef.current?.value) return;
+
+    const inputValue = inputRef.current.value;
+
+    console.log(inputValue);
+
+    inputRef.current.value = '';
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>): void {
+    event.target.setCustomValidity('');
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>): void {
+    event.target.setCustomValidity('Digite uma tarefa!');
+  }
+
   return (
-    <div className={styles.newTask}>
-      <input placeholder='Adicione uma nova tarefa' />
-      <button>
+    <form className={styles.newTask} onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        required
+        onInvalid={handleNewTaskInvalid}
+        placeholder='Adicione uma nova tarefa'
+        onChange={handleNewTaskChange}
+      />
+      <button type='submit'>
         Criar <PlusCircle size={16} weight='bold' />
       </button>
-    </div>
+    </form>
   );
 }
